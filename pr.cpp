a28b39a0352903da2f51626d3bcdbf2c687a94b1
nice sink nah? <97060304+psicho100pro@users.noreply.github.com>
2023-03-08 11:17:31 +0100

    Update pr.cpp

--- pr.cpp
+++ pr.cpp
@@ -1,76 +1,122 @@
 #include "opentx.h"
 
-#define ROOF 8                                   // y
-#define FLOOR (LCD_H - ROOF - 1)                 // y adjustable, it needs to be an even number cus center
-#define HCENTER (ROOF + ((FLOOR - 1 - ROOF) / 2)) // y
-#define HORLLIM 0                                // x
-#define HORDLIM 97                               // x divid, adjustable, it needs to be an odd number cus center
-#define HORRLIM (LCD_W - 1)                      // x    
-#define HORWCENTER ((HORDLIM - 1) / 2)           // x
-#define FOV 42                                   // fe. 45°=30, 40°=32, 35°=36 ,30°=42, 25°=60 - when the horizon touches ROOF or FLOOR // TODO: need to recalculate
-#define SENSORSENSE true                       // sensor identifier
+/*
+!!! SMLSIZE writes only capital letters and nums !!! the raster is too coarse
+due to the lack of space, the effort is to simplify everything
+*/
 
-static const uint8_t lut[] = { // sin table fixed SCALE 6
+#define TOP FH
+#define FOOT (LCD_H - 1) // y adjustable
+#define HRLIM (LCD_W - 1) 
+#define HORLLIM 0
+#define HORDLIM 94 // x divid, adjustable
+#define HORTOPL (TOP + 1)
+#define HORFOOTL (FOOT - 1)
+#define HORHCENTER (TOP + ((FOOT - 1 - TOP) / 2))
+#define HORWCENTER (HORDLIM / 2)
+
+#define FOV 44    //fe. 50°=32, 45°=36, 40°=40,	, 35°=44,	, 30°=48,	, 25°=52 - when the horizon touches TOP or FOOT
+
+#define RSSI  telemetryData.rssi.value
+#define RQly (telemetryItems[2].value)
+#define TPWR (telemetryItems[6].value)
+//#define GPS  (telemetryItems[10].value) //coord
+#define Spd  (telemetryItems[11].value)
+//#define Hdg  (telemetryItems[12].value)
+#define Alt  (telemetryItems[13].value)
+#define Sats (telemetryItems[14].value)
+#define RxBt (telemetryItems[15].value)
+#define Curr (telemetryItems[16].value)
+#define Cons (telemetryItems[17].value) // consum
+#define Batp (telemetryItems[18].value) // Bat%
+#define pitch (telemetryItems[19].value)
+#define roll (telemetryItems[20].value)
+//#define yaw  (telemetryItems[21].value)
+#define STAB (telemetryItems[22].value)
+
+static const uint16_t lut[] = { // sin table fixed SCALE 6
 0,1,2,3,4,6,7,8,9,10,
 11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,
 32,33,34,35,36,37,38,39,39,40,41,42,43,44,44,45,46,47,48,48,
 49,50,50,51,52,52,53,54,54,55,55,56,57,57,58,58,58,59,59,60,
 60,61,61,61,62,62,62,62,63,63,63,63,63,64,64,64,64,64,64,64
 };
-bool tgErrFlag = 0; //tangens 90°, 270°
-int8_t fSinCos(int16_t deg, bool f = 0) // 0=sin 1=cos
+bool tgEfl; // tangens error flag
+bool upSdo; // upsidedown 1
+int8_t fSinCos(int16_t deg, bool f = false) // 0=sin 1=cos
 {
-  uint16_t ab = abs(f ? deg + (uint8_t)90 : deg);
-  int8_t ret;
-  if (ab < 90) { ret = lut[ab % 90]; }
-  else if (ab < 180) { ret = lut[90 - ab % 90]; }
-  else if (ab < 270) { ret = -lut[ab % 90]; }
-  else { ret = -lut[90 - ab % 90]; }
-  if (ab == 90 || ab == 270) tgErrFlag = 1;
-  return (deg >= 0) ? ret : -ret;
+  if (deg == 0) return f ? lut[89] : 0;
+  deg = f ? deg + 157 : deg;
+  deg = deg * 180 / 314;
+  if (deg >= 360) deg -= 360;
+  uint16_t ab = abs(deg);
+  if (!f) tgEfl = (ab == 90 || ab == 270) ? true : 0;
+
+  if (ab == 180) return 0;
+  if (deg == -270 || (f ? deg == 90 : ab == 90)) return lut[89];
+  if (deg == -90 || (f ? deg == 270 : ab == 270)) return -lut[89];
+
+  if (ab < 90) { return lut[ab % 90] * (deg < 0 ? -1 : 1);
+  } else if (ab < 180) { return lut[90 - ab % 90] * (deg < 0 ? -1 : 1);
+  } else if (ab < 270) { return -lut[ab % 90] * (deg < 0 ? -1 : 1);
+  } else { return -lut[90 - ab % 90] * (deg < 0 ? -1 : 1); }
 };
-uint8_t hYlimit(int8_t val) // y over limit
+
+
+uint8_t hYlimit(uint16_t val) // y over limit
 {
   if (ROOF >= val) return ROOF + 1;
   if (val >= FLOOR) return FLOOR - 1;
   return val;
 };
 
-uint8_t i = 0;
+void lcdDrawAngLine(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t ahcy, int8_t sin, int8_t cos)
+{ //coord_t ahcx = HORWCENTER
+  x1 = ((((x1 - HORWCENTER) << 6) * cos) >> 12) + ((((y1 - ahcy) << 6) * sin) >> 12) + HORWCENTER;
+  y1 = ((((x1 - HORWCENTER) << 6) * -sin) >> 12) + ((((y1 - ahcy) << 6) * cos) >> 12) + ahcy;
+  x2 = ((((x2 - HORWCENTER) << 6) * cos) >> 12) + ((((y2 - ahcy) << 6) * sin) >> 12) + HORWCENTER;
+  y2 = ((((x2 - HORWCENTER) << 6) * -sin) >> 12) + ((((y2 - ahcy) << 6) * cos) >> 12) + ahcy;
+  lcdDrawLine(x1, y1, x2, y2, DOTTED);
+};
 
 void hello_draw() {
 
-// ROOF bar:
-lcdDrawSolidFilledRect(0, 0, HORRLIM, ROOF);
+// TOP bar:
+lcdDrawFilledRect(0, 0, HRLIM, TOP, SOLID);
 putsVolts(1, 1, g_vbat100mV, SMLSIZE | INVERS | (IS_TXBATT_WARNING() ? BLINK : 0));
 lcdDrawText(lcdNextPos + 1, 1, "TW:", SMLSIZE | INVERS);
-lcdDrawNumber(lcdNextPos, 1, telemetryItems[6].value, SMLSIZE | INVERS);
-putsModelName(lcdNextPos + 2, 1, g_model.header.name, g_eeGeneral.currModel, TINSIZE | INVERS);
-//lcdDrawSizedText(lcdNextPos + 2, 1, g_model.flightModeData[mixerCurrentFlightMode].name, sizeof(g_model.flightModeData[mixerCurrentFlightMode].name), TINSIZE | INVERS);
-//drawFlightMode(lcdNextPos + 2, 1, mixerCurrentFlightMode, TINSIZE | INVERS);
-if (g_model.timers[0].mode){
-  lcdDrawText(HORRLIM - 33, 1, "T1:", SMLSIZE | INVERS);
-  drawTimer(HORRLIM - 24, 1, timersStates[0].val, SMLSIZE |INVERS | (timersStates[0].val < 0 ? BLINK : 0));
-};
-if (g_model.timers[1].mode){
-  lcdDrawText(HORRLIM - 33, 1, "T2:", SMLSIZE | INVERS);
-  drawTimer(HORRLIM - 24, 1, timersStates[1].val, SMLSIZE | INVERS | (timersStates[1].val < 0 ? BLINK : 0));
-};
-
-// margin line
-lcdDrawLine(HORDLIM, ROOF, HORDLIM, FLOOR); // divid
+lcdDrawNumber(lcdNextPos, 1, TPWR, SMLSIZE | INVERS);
+putsModelName(lcdNextPos + 2, 1, g_model.header.name, g_eeGeneral.currModel, SMLSIZE | INVERS);
+if (g_model.timers[0].mode) {
+    lcdDrawText(HORDLIM - FW - 4, 1, "T1:", SMLSIZE | INVERS);
+    drawTimer(lcdNextPos + FW, 1, timersStates[0].val, SMLSIZE | INVERS | (timersStates[0].val < 0 ? BLINK : 0));
+  };
+if (g_model.timers[1].mode) {
+    lcdDrawText(HORDLIM - FW - 4, 1, "T2:", SMLSIZE | INVERS);
+    drawTimer(lcdNextPos + FW, 1, timersStates[1].val, SMLSIZE | INVERS | (timersStates[1].val < 0 ? BLINK : 0));
+  };
+// margin line:
+lcdDrawLine(HORDLIM + 1, TOP, HORDLIM + 1, FOOT); // divid
+// vario ?
+lcdDrawLine(HORDLIM + 4, TOP, HORDLIM + 4, FOOT); // divid
 
 // a.horizon:
-// TODO: the known phenomenon when turning upside down, the orientation is reversed within 180 degrees , it needs to be treated somehow
 // pitch
-uint8_t ahcy = HCENTER;
-ahcy = hYlimit(((uint16_t(FOV << 6) * -fSinCos(telemetryItems[19].value)) >> 12) + HCENTER);
+int16_t ahcy = HORHCENTER;
+ahcy += (uint16_t(FOV << 6) * -fSinCos(pitch)) >> 12;
+  
+//  TODO: the known phenomenon when turning upside down, the orientation is reversed within 180 degrees roll and 90 pitch
+
+//top
+drawFlightMode(HORWCENTER - 2, TOP + 2, mixerCurrentFlightMode, SMLSIZE | RIGHT);
+drawValueWithUnit(HORWCENTER + 2, TOP + 2, STAB, UNIT_TEXT , SMLSIZE);
+lcdDrawText(HORDLIM - 3 * FWNUM, TOP + 2, "S:", SMLSIZE);
+lcdDrawNumber(lcdNextPos, TOP + 2, Sats, SMLSIZE | (Sats < 5 ? BLINK : 0));
+
 // roll
-tgErrFlag = 0; // tangens 90°, 270°
-int8_t sin = fSinCos(telemetryItems[20].value);
-int8_t cos = fSinCos(telemetryItems[20].value, 1);
-int16_t tan = (sin * (1 << 6) / (tgErrFlag ? (uint8_t)tgErrFlag : cos));
-tan = (telemetryItems[20].value < 0) ? -tan : tan;
+int8_t sin = fSinCos(roll);
+int8_t cos = fSinCos(roll, true);
+int16_t tan = (sin * (1 << 6) / (tgEfl ? 1 : cos));
 
 // ground fill
 for (uint8_t x = HORLLIM + 1; x < HORWCENTER; x++)
@@ -80,56 +126,38 @@ for (uint8_t x = HORLLIM + 1; x < HORWCENTER; x++)
 };
 lcdDrawLine(HORWCENTER, ahcy, HORWCENTER, FLOOR - 1); // mid
 
-// crosshair :D
-lcdDrawLine(HORWCENTER, HCENTER - 1, HORWCENTER, HCENTER + 1);
-lcdDrawLine(HORWCENTER - 1, HCENTER, HORWCENTER + 1, HCENTER);
-lcdDrawLine(HORWCENTER - 6, HCENTER, HORWCENTER - 6, HCENTER + 3);
-lcdDrawLine(HORWCENTER + 6, HCENTER, HORWCENTER + 6, HCENTER + 3);
-lcdDrawLine(HORWCENTER + 7, HCENTER, HORWCENTER + 15, HCENTER);
-lcdDrawLine(HORWCENTER - 15, HCENTER, HORWCENTER - 7, HCENTER);
-
-// yaw
-lcdDrawFilledRect(HORWCENTER - 2 * FWNUM + 2, FLOOR - FH, FWNUM * 3 + 1, 8, SOLID, ERASE);
-lcdDrawNumber(HORWCENTER - 2 * FWNUM + 3, FLOOR - FH + 1, telemetryItems[21].value, SMLSIZE);
+// pitch scale line
+for (uint8_t l = 1; l < 4; l++)
+{
+  lcdDrawAngLine(HORWCENTER - ((l == 2) ? 11 : 3), ahcy - uint8_t(FOV / 8 * l), HORWCENTER + ((l == 2) ? 11 : 3), ahcy - uint8_t(FOV / 8 * l), ahcy, sin, cos);
+  lcdDrawAngLine(HORWCENTER - ((l == 2) ? 11 : 3), ahcy + uint8_t(FOV / 8 * l), HORWCENTER + ((l == 2) ? 11 : 3), ahcy + uint8_t(FOV / 8 * l), ahcy, sin, cos);
+};
+  
+ // crosshair :D
+lcdDrawLine(HORWCENTER, HORHCENTER - 1, HORWCENTER, HORHCENTER + 1);
+lcdDrawLine(HORWCENTER - 1, HORHCENTER, HORWCENTER + 1, HORHCENTER);
+lcdDrawLine(HORWCENTER - 6, HORHCENTER, HORWCENTER - 6, HORHCENTER + 3);
+lcdDrawLine(HORWCENTER + 6, HORHCENTER, HORWCENTER + 6, HORHCENTER + 3);
+lcdDrawLine(HORWCENTER + 7, HORHCENTER, HORWCENTER + 15, HORHCENTER);
+lcdDrawLine(HORWCENTER - 15, HORHCENTER, HORWCENTER - 7, HORHCENTER);
 
 // side indicators
-lcdDrawFilledRect(HORLLIM, HCENTER - 5, FWNUM * 3 + 3, 10, SOLID, FORCE);
-lcdDrawFilledRect(HORLLIM + 1, HCENTER - 4, FWNUM * 3 + 1, 8, SOLID, ERASE);
-lcdDrawNumber(HORLLIM + 2, HCENTER - 3, telemetryItems[11].value, SMLSIZE);
-lcdDrawFilledRect(HORDLIM - FWNUM * 3 - 2, HCENTER - 5, FWNUM * 3 + 2, 10, SOLID, FORCE);
-lcdDrawFilledRect(HORDLIM - FWNUM * 3 - 1, HCENTER - 4, FWNUM * 3 + 1, 8, SOLID, ERASE);
-lcdDrawNumber(HORDLIM - FWNUM * 3, HCENTER - 3, telemetryItems[13].value, SMLSIZE);
+lcdDrawFilledRect(HORLLIM, HORHCENTER - 5, 3 * FWNUM + 3, FH + 2, SOLID, FORCE);
+lcdDrawFilledRect(HORLLIM + 1, HORHCENTER - 4, 3 * FWNUM + 1, FH, SOLID, ERASE);
+lcdDrawNumber(HORLLIM + 2, HORHCENTER - 3, Spd, SMLSIZE);
+lcdDrawFilledRect(HORDLIM - 3 * FWNUM - 1 , HORHCENTER - 5, 3 * FWNUM + 3, FH + 2 , SOLID, FORCE);
+lcdDrawFilledRect(HORDLIM - 3 * FWNUM, HORHCENTER - 4, 3 * FWNUM + 1, FH, SOLID, ERASE);
+lcdDrawNumber(HORDLIM - 3 * FWNUM + 1, HORHCENTER - 3, Alt, SMLSIZE);
 
-// rigth block: // not completed
-lcdDrawText(HORDLIM + 5, ROOF + 2, "S:", SMLSIZE);
-drawValueWithUnit(lcdNextPos + 4, ROOF + 2, telemetryItems[14].value, UNIT_RAW, SMLSIZE | RIGHT| (telemetryItems[14].value < 5 ? BLINK : 0));
-drawValueWithUnit(HORDLIM + 10, ROOF + 10, telemetryItems[16].value, UNIT_AMPS, SMLSIZE | RIGHT);
-drawValueWithUnit(HORDLIM + 10, ROOF + 20, telemetryItems[17].value, UNIT_MAH, SMLSIZE | RIGHT);
-
-// FLOOR bar:
-lcdDrawSolidFilledRect(0, FLOOR + 1, HORRLIM, FLOOR + 1);
-putsVolts(1, FLOOR + 2, telemetryItems[15].value, SMLSIZE | INVERS);
-lcdDrawText(lcdNextPos + 2, FLOOR + 2, "RS:", SMLSIZE | INVERS);
-lcdDrawNumber(lcdNextPos, FLOOR + 2, telemetryData.rssi.value, SMLSIZE | INVERS ); // | (telemetryData.rssi.value < g_model.rssiAlarms.getWarningRssi()) ? BLINK : 0);
-lcdDrawText(lcdNextPos + 2, FLOOR + 2, "RQ:", SMLSIZE | INVERS);
-lcdDrawNumber(lcdNextPos , FLOOR + 2, telemetryItems[2].value, SMLSIZE | INVERS);
-drawValueWithUnit(HORRLIM, FLOOR + 2, telemetryItems[22].value, UNIT_TEXT, SMLSIZE | INVERS | RIGHT);
-
-#if defined(SENSORSENSE)
-if (isTelemetryFieldAvailable(i)){
-  TelemetrySensor &sensor = g_model.telemetrySensors[i];
-  char sensorName[TELEM_LABEL_LEN + 7];
-  zchar2str(sensorName, sensor.label, TELEM_LABEL_LEN);
-  lcdDrawNumber(5, 15, i, SMLSIZE);
-  lcdDrawText(lcdNextPos + 3 , 15, sensorName, SMLSIZE);
-  drawValueWithUnit(lcdNextPos, 15, telemetryItems[i].value, sensor.unit, SMLSIZE);
-}else{
-  lcdDrawNumber(5, 15, i, SMLSIZE);
-  lcdDrawText(10, 15, "NENI", SMLSIZE);
-};
-#endif
-}
-
+// rigth block:
+putsVolts(HORDLIM + FW, HORTOPL + FH, RxBt, SMLSIZE);
+lcdDrawNumber(HORDLIM + FW, HORTOPL + 15, Curr, SMLSIZE);
+lcdDrawText(lcdNextPos, HORTOPL + 15, "A", SMLSIZE);
+lcdDrawNumber(HORDLIM + FW, HORTOPL + 22, Cons, SMLSIZE);
+lcdDrawText(HORDLIM + 2 * FW , HORTOPL + 29, "mAh", SMLSIZE);
+lcdDrawText(HORDLIM + FW, FOOT - 13, "S:", TINSIZE);
+lcdDrawText(HORDLIM + FW, FOOT - 6, "Q:", TINSIZE);
+  
 void hello_stop()
 {
   popMenu();
@@ -138,12 +166,10 @@ void hello_run(event_t event)
 {
   if (event == EVT_KEY_FIRST(KEY_UP))
   {
-     i++;
-     AUDIO_TRIM_MAX();
+    AUDIO_TRIM_MAX()
   }
   if (event == EVT_KEY_FIRST(KEY_DOWN))
   {
-    i--;
     AUDIO_TRIM_MIN();
   }
    if (event == EVT_KEY_FIRST(KEY_ENTER))
